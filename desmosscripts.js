01a5b54b8139f74b7078c81370561ac86e1f5713
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2020-02-20 22:32:58 -0800

    Starting on a graphing...kinda...web app!  Will use CanvasJS tomorrow.

---
+++ desmosscripts.js
@@ -0,0 +1,9 @@
+const calc = () => {
+  var xx = xvalue.value
+  var yy = xvalue.value
+  var newLine = document.createElement("p")
+  var coordinates = document.createTextNode(`${xx},${yy}`)
+  var area = document.getElementById("outputcoordinates")
+  area.appendChild
+
+}
